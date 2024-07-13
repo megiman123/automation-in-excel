8fb56861fb90d2d76374807eb5e1dcd4e52196a3
megiman123 <tiwarishambhav625@gmail.com>
2024-07-13 22:43:42 +0530

    Adding a basic and shitty graph

--- main.[y.py
+++
@@ -1,5 +0,0 @@
-import openpyxl as xl
-wb = xl.load_workbook('Book1.xlsx')
-sheet = wb['Sheet1']
-cell = sheet.cell(1,1)
-print(cell.value)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import openpyxl as xl
 from openpyxl.chart import Reference,BarChart
-wb = xl.load_workbook('Book1.xlsx')
+wb = xl.load_workbook('sheet.xlsx')
 sheet = wb['Sheet1']
 cell = sheet.cell(1,1)
 for row in range (2,sheet.max_row+1):
@@ -8,4 +8,13 @@ for row in range (2,sheet.max_row+1):
     correct = cell.value * 0.9
     corrected_cell = sheet.cell(row,4)
     corrected_cell.value = correct
-wb.save('Book2.xlsx')
+values = Reference(sheet,
+          min_row=2,
+          max_row=sheet.max_row
+          ,min_col=4,
+          max_col=4
+          )
+chart =  BarChart()
+chart.add_data(values)
+sheet.add_chart(chart,'e2')
+wb.save('sheet2.xlsx')
